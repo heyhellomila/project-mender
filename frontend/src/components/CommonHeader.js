bdee7120db9ce398ab5c5b2634f4560067a7640a
Songtone <s.ongtone@hotmail.com>
2019-11-05 17:55:00 -0500

    Refactoring

--- frontend/src/components/CommonHeader.js
+++ frontend/src/components/CommonHeader.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text, Button, Image } from 'react-native';
 import Search from 'react-native-search-box';
+import { headerStyles} from '../stylesheets/Stylesheet';
 
 export default class CommonHeader extends Component {
     render() {
         return (
-            <View style={{ flex: 1, borderBottomColor:'#d3d3d3', borderBottomWidth:2}}>
-               <View style={{ flex: 3 }}><Text style={{ textAlign: 'center', fontSize: 25 }}>Hi {this.props.user.firstName}</Text></View>
-                <View style={{ flex: 3, alignSelf:'center', width: '75%' }}><Search ref="search_box"></Search></View>
+            <View style={headerStyles.commonHeaderComponent}>
+               <View style={headerStyles.commonHeaderTextComponent}><Text style={ headerStyles.commonHeaderText}>Hi {this.props.user.firstName}</Text></View>
+                <View style={ headerStyles.commonHeaderSearch }><Search ref="search_box"></Search></View>
             </View>
         );
     }
--- frontend/src/stylesheets/Stylesheet.js
+++ frontend/src/stylesheets/Stylesheet.js
@@ -29,6 +29,26 @@ const loginComponent = StyleSheet.create({
     }
 })
 
+const headerStyles = StyleSheet.create({
+    commonHeaderTextComponent:{
+        flex: 3
+    },
+    commonHeaderText:{
+        textAlign: 'center', 
+        fontSize: 25
+    },
+    commonHeaderSearch:{
+        flex: 3, 
+        alignSelf:'center', 
+        width: '75%' 
+    },
+    commonHeaderComponent:{
+        flex: 1, 
+        borderBottomColor:'#d3d3d3', 
+        borderBottomWidth:2
+    }
+})
+
 const jobListTable = StyleSheet.create({
     jobListTableContainer: {flex: 5, padding: 16, paddingTop: 30, backgroundColor: '#fff'},
     jobListTablehead: { height: 40, backgroundColor: '#f1f8ff' },
@@ -49,4 +69,4 @@ const buttons = StyleSheet.create({
     }
 })
 
-export { styles, buttons, loginComponent, jobListTable } 
+export { styles, buttons, loginComponent, jobListTable, headerStyles } 
