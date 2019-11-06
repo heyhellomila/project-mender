03912355f943eef0eb66aa3c919c09b681c9b016
Jamal <jamal.g16@hotmail.com>
2019-11-06 13:46:44 -0500

    weird fix

--- frontend/src/apis/UserApis.js
+++ frontend/src/apis/UserApis.js
@@ -4,8 +4,8 @@ import { AsyncStorage } from 'react-native';
 export const login = async (email, password) => {
     return await api.post('/users/login', {
         email: email,
-        password: password}
-    );
+        password: password
+    });
 }
 
 export const getUser = async (id) => {
