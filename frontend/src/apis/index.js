6136d941f7f9e38e541fac8a48eb5a149953d8d1
Jamal <jamal.g16@hotmail.com>
2019-11-06 19:33:39 -0500

    another fix??

--- frontend/src/apis/index.js
+++ frontend/src/apis/index.js
@@ -5,4 +5,8 @@ var api = axios.create({
     baseURL: `http://${LOCAL_API_KEY}/api`
 });
 
+api.interceptors.request.use(() => {
+    alert(JSON.stringify(LOCAL_API_KEY));
+});
+
 export default api;
