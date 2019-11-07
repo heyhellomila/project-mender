ee5ba9c987e3f38fdac176070a988a18c2ec7c61
Jamal <jamal.g16@hotmail.com>
2019-11-06 19:36:00 -0500

    ??

--- frontend/src/apis/index.js
+++ frontend/src/apis/index.js
@@ -1,12 +1,8 @@
 import axios from 'axios';
 import { LOCAL_API_KEY } from 'react-native-dotenv'
 
-var api = axios.create({
+export var api = axios.create({
     baseURL: `http://${LOCAL_API_KEY}/api`
-});
-
-api.interceptors.request.use(() => {
-    alert(JSON.stringify(LOCAL_API_KEY));
 });
 
 export default api;
