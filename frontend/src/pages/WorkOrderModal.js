f4b589a80bc5eee787580bc813eb49ccb22a50d2
DanielPri <privorotskyd@gmail.com>
2019-11-09 17:35:56 -0500

    [#19] basis for work order

--- frontend/src/pages/WorkOrderModal.js
+++ frontend/src/pages/WorkOrderModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Modal, Text, TextInput } from 'react-native';
+import { View, Modal, Text, TextInput, Switch, Button } from 'react-native';
 import { connect } from 'react-redux';
 import { ButtonComponent } from '../components/ButtonComponent';
 
@@ -13,35 +13,77 @@ class WorkOrderModal extends React.Component {
     };
     render() {
         return (
+            //buttonFunction={() => this.props.navigation.goBack(null)
             <Modal transparent={true}
             visible={this.state.isVisible}
             onRequestClose={this.closeModal}>
-                <View style={{
-                    flex: 1,
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center'}}>
-                    <View style={{
-                        width: 300,
-                        height: 150}}>
-                        <ButtonComponent
-                            buttonColor='#f56942'
-                            buttonName='Close Modal'
-                            buttonFunction={() => this.props.navigation.goBack(null)}
-                            />   
-                        <View>
-                          <Text>Corrective Preventive</Text>
-                          <Text>Sector<TextInput 
-                                                style={{ width:100, height: 40, borderColor: 'gray', borderWidth: 1 }}
-                                                />
-                                                </Text>
-                          <Text>C</Text>
-                          <Text>D</Text>
-                          <Text>E</Text>
+                    
+                    <View style={styles.elementsContainer}>
+                        <Button 
+                        style={{alignSelf:'flex-end', position:'absolute'}}
+                        title="close"
+                        onPress ={() => this.props.navigation.goBack(null)}/>
+                    <Text style={styles.headerStyle}>Work Order</Text>
+                    <Text style={{fontSize:13, alignSelf:'center'}}>Property Number and Property Adress</Text>
+                        <View style={{flexDirection:'row', alignSelf: 'center'}}>
+                            <View style={{width: 100, height: 50, justifyContent:'center'}}>
+                                <Text style={{textAlign:'center', backgroundColor: '#a3a3a3', borderRadius:5}}>Corrective</Text>
+                            </View>
+                            <View style={{width: 100, height: 50, justifyContent:'center'}}>
+                                <Text style={{textAlign:'center', backgroundColor: '#a3a3a3', borderRadius:5}}>Preventive</Text>
+                            </View>
                         </View>
 
+                        <View style={{flexDirection:'row', alignSelf: 'center'}}>
+                            <View style={{width: 100, height: 50, justifyContent:'center'}}>
+                                <Text style={{textAlign:'center', backgroundColor: '#00ace6', borderRadius:5}}>Sector</Text>
+                            </View>
+                            <View style={{width: 100, height: 50, justifyContent:'center'}}>
+                                <Text style={{textAlign:'center', backgroundColor: '#ffffff', borderRadius:5}}>Item picker goes here </Text>
+                            </View>
+                        </View>
+
+                        <View style={{flexDirection:'row', alignSelf: 'center'}}>
+                            <View style={{width: 100, height: 50, justifyContent:'center'}}>
+                                <Text style={{textAlign:'center', backgroundColor: '#00ace6', borderRadius:5}}>Title</Text>
+                            </View>
+                            <View style={{width: 100, height: 50, justifyContent:'center'}}>
+                                <TextInput style={{textAlign:'center', backgroundColor: '#ffffff', borderRadius:5}}/>
+                            </View>
+                        </View>
+
+                        <View style={{flexDirection:'row', alignSelf: 'center'}}>
+                            <View style={{width: 100, height: 50, justifyContent:'center'}}>
+                                <Text style={{textAlign:'center', backgroundColor: '#00ace6', borderRadius:5}}>Cause</Text>
+                            </View>
+                            <View style={{width: 100, height: 50, justifyContent:'center'}}>
+                                <TextInput style={{textAlign:'center', backgroundColor: '#ffffff', borderRadius:5}}/>
+                            </View>
+                        </View>
+                        
+                        <View style={{flexDirection:'row', alignSelf: 'center'}}>
+                            <View style={{width: 100, height: 50, justifyContent:'center'}}>
+                                <Text style={{textAlign:'center', backgroundColor: '#00ace6', borderRadius:5}}>Service needed?</Text>
+                            </View>
+                            <View style={{width: 100, height: 50, justifyContent:'center'}}>
+                                <Switch style={{alignSelf:'center'}}/>
+                            </View>
+                        </View>
+
+                        <View style={{flexDirection:'row', alignSelf: 'center'}}>
+                            <View style={{width: 100, height: 50, justifyContent:'center'}}>
+                                <Text style={{textAlign:'center', backgroundColor: '#00ace6', borderRadius:5}}>Urgent?</Text>
+                            </View>
+                            <View style={{width: 100, height: 50, justifyContent:'center'}}>
+                                <Switch style={{alignSelf:'center'}}/>
+                            </View>
+                        </View>
+
+                        <TextInput  
+                        style={{width:200, height: 150, backgroundColor: '#ffffff', alignSelf: 'center', borderRadius:5}}
+                        placeholder="Notes/additional information"/>
+
                     </View>
-                </View>
             </Modal>
         );
     }
@@ -52,3 +94,22 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps, null)(WorkOrderModal);
+
+const styles = {
+    headerStyle: {
+      fontSize: 24,
+      textAlign: 'center',
+      fontWeight: '100',
+      marginTop: 48,
+      marginBottom: 24
+    },
+    elementsContainer: {
+      flex: 1,
+      backgroundColor: '#ecf5fd',
+      marginLeft: 24,
+      marginRight: 24,
+      marginBottom: 24,
+      marginTop: 24
+    }
+  
+}
