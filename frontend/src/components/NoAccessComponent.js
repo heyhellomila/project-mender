bbf25b0a977438ec2240c64dccca9508b33a2b88
Jamal <jamal.g16@hotmail.com>
2019-11-16 16:33:46 -0500

    [#74] Removed unused code and refactored

--- frontend/src/components/NoAccessComponent.js
+++ frontend/src/components/NoAccessComponent.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Button, Icon } from 'react-native-elements';
 import { noAccessComponent } from '../stylesheets/NoAccessComponent';
 
 const NoAccessComponent = (props) => {
     return (
         <View style={noAccessComponent.container}>
-            <Text style={noAccessComponent.text}>
-                {props.errorMessage}
-            </Text>
-            <Button
-                style={noAccessComponent.button}
-                title="Go back"
-                onPress={() => props.navigation.goBack()}/>
+            <View style={noAccessComponent.rowContainer}>
+                <Text style={noAccessComponent.text}>
+                    {props.errorMessage}
+                </Text>
+            </View>
+            <View style={noAccessComponent.rowContainer}>
+                <Button
+                    style={noAccessComponent.button}
+                    title="Go back"
+                    onPress={() => props.navigation.goBack()}/>
+            </View>
         </View>
     );
 };
