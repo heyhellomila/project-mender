d46e18202dc48f59ff9be077a4f0104bdf3ef018
Songtone <s.ongtone@hotmail.com>
2020-01-16 03:18:49 -0500

    fixing issue with naming

--- frontend/src/components/profileForms/ProfilePageComponent.js
+++ frontend/src/components/profileForms/ProfilePageComponent.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity, Button} from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View, TextInput, Image, TouchableOpacity, Button} from 'react-native';
 import {buttonStyles, containerStyles, imageStyles, profilePageStyles} from '../../stylesheets/ProfilePageStylesheet';
 
 const profilePicture = require('../../../assets/profileMender.png');
@@ -35,60 +35,60 @@ const ProfilePageComponent = (props) => {
                             <Text>
                                 <Text style={profilePageStyles.profilePageBoldText}>Email: </Text>
                                 <Text>{props.user.email}</Text>
-                        </Text>
+                            </Text>
+                        </View>
+                        <View style={profilePageStyles.profilePageJustifyContentEnd}>
+                            <TouchableOpacity
+                                onPress={() => props.goToUpdateEmailPage()}><Text
+                                style={profilePageStyles.profilePageButtonText}>EDIT</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                    <View style={profilePageStyles.profilePageJustifyContentEnd}>
-                        <TouchableOpacity
-                            onPress={() => props.goToUpdateEmailPage()}><Text
-                            style={profilePageStyles.profilePageButtonText}>EDIT</Text>
-                        </TouchableOpacity>
+                </View>
+                <View style={profilePageStyles.profilePageRows}>
+                    <View style={profilePageStyles.profilePageInsideRows}>
+                        <View style={profilePageStyles.profilePageJustifyContentStart}>
+                            <Text>
+                                <Text style={profilePageStyles.profilePageBoldText}>Phone Number: </Text>
+                                <Text>{props.user.phoneNumber}</Text>
+                            </Text>
+                        </View>
+                        <View style={profilePageStyles.profilePageJustifyContentEnd}>
+                            <TouchableOpacity
+                                onPress={() => props.goToUpdatePhoneNumberPage()}><Text
+                                style={profilePageStyles.profilePageButtonText}>EDIT</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+                <View style={profilePageStyles.profilePageRows}>
+                    <View style={profilePageStyles.profilePageInsideRows}>
+                        <View style={profilePageStyles.profilePageJustifyContentStart}>
+                            <Text>
+                                <Text style={profilePageStyles.profilePageBoldText}>Password: </Text>
+                                <Text>********</Text>
+                            </Text>
+                        </View>
+                        <View style={profilePageStyles.profilePageJustifyContentEnd}>
+                            <TouchableOpacity
+                                onPress={() => props.goToChangePasswordPage()}><Text
+                                style={profilePageStyles.profilePageButtonText}>EDIT</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </View>
             </View>
-            <View style={profilePageStyles.profilePageRows}>
-                <View style={profilePageStyles.profilePageInsideRows}>
-                    <View style={profilePageStyles.profilePageJustifyContentStart}>
-                        <Text>
-                            <Text style={profilePageStyles.profilePageBoldText}>Phone Number: </Text>
-                            <Text>{props.user.phoneNumber}</Text>
-                        </Text>
-                    </View>
-                    <View style={profilePageStyles.profilePageJustifyContentEnd}>
-                        <TouchableOpacity
-                            onPress={() => props.goToUpdatePhoneNumberPage()}><Text
-                            style={profilePageStyles.profilePageButtonText}>EDIT</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </View>
-            <View style={profilePageStyles.profilePageRows}>
-                <View style={profilePageStyles.profilePageInsideRows}>
-                    <View style={profilePageStyles.profilePageJustifyContentStart}>
-                        <Text>
-                            <Text style={profilePageStyles.profilePageBoldText}>Password: </Text>
-                            <Text>********</Text>
-                        </Text>
-                    </View>
-                    <View style={profilePageStyles.profilePageJustifyContentEnd}>
-                        <TouchableOpacity
-                            onPress={() => props.goToChangePasswordPage()}><Text
-                            style={profilePageStyles.profilePageButtonText}>EDIT</Text>
-                        </TouchableOpacity>
-                    </View>
+            <View style={profilePageStyles.profilePageBackHome}>
+                <View style={profilePageStyles.profilePageJustifyContentEnd}>
+                    <TouchableOpacity
+                        onPress={() => props.goToHomePage()}><Text style={profilePageStyles.profilePageButtonText}>BACK TO
+                        HOME</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
         </View>
-    <View style={profilePageStyles.profilePageBackHome}>
-        <View style={profilePageStyles.profilePageJustifyContentEnd}>
-            <TouchableOpacity
-                onPress={() => props.goToHomePage()}><Text style={profilePageStyles.profilePageButtonText}>BACK TO
-                HOME</Text>
-            </TouchableOpacity>
-        </View>
-    </View>
-</View>
-)
-    ;
+    )
+        ;
 }
 
-export default ProfilePageComponent;
+export default ProfilePageComponent; 
