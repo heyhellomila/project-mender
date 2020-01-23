e3bc5a0638f7f604c03e8ccd97e650d5b62f6ad5
Dania Kalomiris <daniakalomiris@gmail.com>
2020-01-22 23:45:49 -0500

    [#178] Add buttons to bookmarked filter

--- frontend/src/components/jobListPage/filterModals/BookmarkedFilterComponent.js
+++ frontend/src/components/jobListPage/filterModals/BookmarkedFilterComponent.js
@@ -9,19 +9,33 @@ const BookmarkedFilterComponent = (props) => {
             isVisible={props.isBookmarkedModalVisible}
             backdropColor={'white'}
             animationInTiming={0}
-            animationOutTiming={0}
+            animationOutTiming={100}
             backdropTransitionInTiming={0}
             backdropTransitionOutTiming={0}
             backdropOpacity={1}
             style={{justifyContent: 'flex-end', margin: 0}}
         >
-            <View style={{flex: 1, alignContent: 'center', justifyContent: 'center'}}>
-                <Text>NEW BOOKMARKED</Text>
-                <Button 
-                    title='Save' 
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'baseline', marginTop: 25}}>
+                <Button
+                    title='Cancel'
                     onPress={props.toggleBookmarkedModal}
                 />
+                <Text>Filter</Text>
+                <Button
+                    title='Reset'
+                    onPress={props.toggleBookmarkedModal}
+                />
+            </View>
+            <View style={{flex: 1, alignContent: 'center', justifyContent: 'center'}}>
+                <Text>NEW BOOKMARKED</Text>
             </View> 
+            <View style={{justifyContent: 'flex-end'}}>
+                <Button 
+                    title='Apply' 
+                    onPress={props.toggleBookmarkedModal}
+                    style={{borderRadius: 0, height: 100}}
+                />
+            </View>
         </Modal>
     );
 };
