cddd94b5c7273dd9feda1abd3c386022fb41868b
Songtone <s.ongtone@hotmail.com>
2020-02-08 16:07:09 -0500

    [#221] Removed welcome page files

--- frontend/src/navigation/AppNavigator.js
+++ frontend/src/navigation/AppNavigator.js
@@ -1,6 +1,5 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
-import WelcomePage from '../pages/WelcomePage';
 import SignUpPage from '../pages/SignUpPage';
 import LogInPage from '../pages/LogInPage';
 import DrawerNavigator from './DrawerNavigator';
