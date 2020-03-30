94af74b9b356607bf35e4842eb3ef9c609e7b660
Jamal <jamal.g16@hotmail.com>
2020-03-30 12:57:12 -0400

    [#257] added test to build and render project and fixed config

---
+++ frontend/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    preset: "jest-expo",
+    verbose: true,
+    clearMocks: true,
+    collectCoverage: true,
+    moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+    transform: {
+        '^.+\\.(js|jsx)?$': 'babel-jest'
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1'
+    },
+    setupFiles: [
+        "./node_modules/react-native-gesture-handler/jestSetup.js"
+    ],
+    transformIgnorePatterns: [
+        "node_modules/?!(react-navigation)"
+    ]
+};
